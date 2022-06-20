809dec6f1baafc7c2b827712168c7a4917a7317d
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-20 18:38:23 -0500

    Fix rubocop offenses and gives basic style
    modified:   Gemfile
    modified:   app/assets/stylesheets/application.css
    modified:   app/controllers/application_controller.rb
    modified:   app/controllers/recipes_controller.rb
    modified:   app/views/layouts/application.html.erb
    deleted:    test/controllers/recipies_controller_test.rb

--- test/controllers/recipies_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class RecipiesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get recipies_index_url
-    assert_response :success
-  end
-end
