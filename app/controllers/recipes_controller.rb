39bcf925c7ad3f2ea3d42e18ea7d62e22483bb25
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-21 13:31:24 -0500

    Add actions destroy, new, create, show, toggle to recipes controller
    modified:   app/controllers/recipes_controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,30 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.all.where(user: current_user)
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    flash[:notice] = "#{recipe.name} was successfully deleted"
+    recipe.destroy
+    redirect_to recipes_path
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @ingredients = @recipe.recipe_foods
+  end
+
+  def toggle
+    recipe = Recipe.find(params[:id])
+    puts recipe.public
+    recipe.public = !recipe.public
+    recipe.save
+    recipe.public
+    redirect_to recipe_path(id: params[:id])
+  end
 end
