aebea990ac1c6bf447278db20a7d790d864ac8e7
Mateo Villagomez <mateovillagomez1995@gmail.com>
2022-06-21 00:45:15 -0500

    Add CRUD actions for foods

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,38 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+    @foods = current_user.foods.all.includes(:recipe_foods)
+  end
+
+  def new
+    @food = Food.new
+    @current_user = current_user
+  end
+
+  def create
+    @food = current_user.foods.new(food_params)
+
+    if (food_params[:price].to_f > 0)
+      if @food.save
+        flash[:success] = "#{@food.name} was successfully created"
+        redirect_to foods_url
+      elsif flash[:alert] = "#{@food.name} was not created"
+        redirect_to new_food_url
+      end
+    else
+      flash[:alert] = 'Price must be greater than 0'
+      redirect_to new_food_url
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    flash[:alert] = "#{@food.name} was successfully deleted"
+    @food.destroy
+    redirect_to foods_url
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurementUnit, :price)
+  end
+end
