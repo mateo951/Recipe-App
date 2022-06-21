82a45af8047b8410592144fc68ac7f42d5cddcd7
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-21 13:34:35 -0500

    Add new route to toggle recipe's public status
    modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'recipes#index'
   get 'public_recipes', to: 'public_recipes#index'
-
+  
+  post 'toggle_public', to: 'recipes#toggle', as: 'toggle_public'
+  post 'recipe_delete_ingredient', to: 'recipes#delete_ingredient', as: 'delete_ingredient'
+  
   resources :users
   resources :recipes, only: %i[index show new create destroy] do
     resources :recipe_foods, only: %i[create destroy]
