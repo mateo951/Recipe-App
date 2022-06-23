09b12495d75b3e56ef7bd934b39f57135e6eee8a
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-23 16:26:23 -0500

    Add test for remove button in recipe index
    modified:   app/views/recipes/index.html.erb
    modified:   spec/features/recipes_index_spec.rb

--- spec/features/recipes_index_spec.rb
+++ spec/features/recipes_index_spec.rb
@@ -10,11 +10,15 @@ RSpec.describe 'Recipes page test', type: :feature do
     @public_recipe = Recipe.create(
       name: 'Huevos a la mexicana',
       description: 'The healthy choice',
+      preparationTime: 10,
+      cookingTime: 10,
       user: @user, public: true
     )
     @private_recipe = Recipe.create(
       name: 'Enchiladas',
       description: 'Traditional mexican food',
+      preparationTime: 20,
+      cookingTime: 20,
       user: @user, public: false
     )
   end
@@ -39,11 +43,15 @@ RSpec.describe 'Recipes page test', type: :feature do
     expect((page.has_link?(@public_recipe.name) &&
             page.has_link?(@private_recipe.name))).to be true
   end
-  # it 'The private recipe should not be present' do
-  #   expect(page.has_link?('Enchiladas')).to be false
-  # end
   it "When click on a recipe, it redirects to that recipe's details page" do
     find_link(href: recipe_path(id: @public_recipe.id)).click
     expect(page).to have_content("Preparation time: #{@public_recipe.preparationTime} minutes")
+  end
+  it 'Recipe delete when click on a recipe remove button' do
+    expect((page.has_link?(@public_recipe.name) &&
+            page.has_link?(@private_recipe.name))).to be true
+    click_button('Remove', id: "recipe-#{@public_recipe.id}")
+    expect((page.has_link?(@public_recipe.name) &&
+            page.has_link?(@private_recipe.name))).to be false
   end
 end
