faf3a6be2d2483f3d2b1e7fbdb5eeeaca4d0977e
Mateo Villagomez <mateovillagomez1995@gmail.com>
2022-06-22 19:47:17 -0500

    Add fix styling

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
     @users = User.all
-    @recipes = Recipe.includes([:user]).where(public: true).where.not(user_id: current_user)
+    @recipes = Recipe.includes([:user]).where(public: true)
   end
 end
