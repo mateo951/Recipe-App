d09b79ab7013801aac25c2a8318a112c3b5c798f
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-10-19 14:47:50 -0500

    Remove turbo-stream test

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,4 @@ class Recipe < ApplicationRecord
 
   validates :name, presence: { message: "Name can't be null" }
   validates :description, presence: { message: "Description can't be null" }
-  after_create_commit { broadcast_prepend_to 'recipes', partial: "shared/recipe" }
 end
