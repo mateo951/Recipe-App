0f95196a151ae5e773521f03a8b5f842b6e1178c
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-10-19 14:48:22 -0500

    Add update action and remove toggle_public

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -28,11 +28,10 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @ingredients = @recipe.recipe_foods.includes([:food])
-    @ingredient = RecipeFood.new
     @recipes = Recipe.all
   end
 
-  def toggle_public
+  def update
     @recipe = Recipe.find(params[:id])
     @recipe.public = !@recipe.public
     @recipe.save
@@ -40,63 +39,10 @@ class RecipesController < ApplicationController
     render 'recipes/status_changed'
   end
 
-  def delete_ingredient
-    ingredient = RecipeFood.find(params[:ingredient])
-    recipe = ingredient.recipe_id
-    ingredient.destroy
-    redirect_to recipe_path(id: recipe), notice: "Ingedient: #{ingredient.food.name} deleted", status: 303
-  end
-  
-  def new_ingredient
-    @ingredient = RecipeFood.new
-    @recipe = params[:id]
-    render "add_ingredient"
-  end
-
-  def add_ingredient
-    @recipe = Recipe.find(params[:id])
-    puts "The params are #{params}"
-    @ingredient = RecipeFood.new(permitted_parameters_ingredient)
-    @ingredient.recipe = @recipe
-    if @ingredient.valid?
-      @ingredient.save
-      respond_to do |format|
-        format.turbo_stream
-        format.html {redirect_to recipe_path(id: @recipe.id), status: 303}
-      end
-      return
-    end
-    redirect_to recipe_path(id: @recipe.id), alert: 'There was an error adding the ingredient'
-  end
-
-  def change_ingredient
-    @ingredient = RecipeFood.find(params[:id])
-    render 'modify_ingredient'
-  end
-
-  def update_ingredient
-    ingredient_id = params[:id]
-    quantity = params[:recipe_food][:quantity]
-    ingredient = RecipeFood.find(ingredient_id)
-    ingredient.quantity = quantity
-    if ingredient.valid?
-      ingredient.save
-      flash.now[:notice]= "ok"
-      redirect_to recipe_path(id: ingredient.recipe.id), status: 303
-      return
-    end
-    redirect_to recipe_path(id: params[:recipe_id]), status: 303,
-                alert: 'There was an error changing the ingredient'
-  end
-
   private
 
   def permitted_parameters_recipe
     params.require(:recipe).permit(:name, :description, :cookingTime,
                                    :preparationTime, :public)
-  end
-
-  def permitted_parameters_ingredient
-    params.require(:recipe_food).permit(:food_id, :quantity)
   end
 end
