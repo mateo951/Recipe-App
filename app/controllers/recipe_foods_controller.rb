a017252436338feb38c981c236b2256f34c81e8e
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-10-21 23:08:33 -0500

    Add turbo-stream response for update ingredient

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -36,11 +36,14 @@ class RecipeFoodsController < ApplicationController
   def update
     ingredient_id = params[:id]
     quantity = params[:recipe_food][:quantity]
-    ingredient = RecipeFood.find(ingredient_id)
-    ingredient.quantity = quantity
-    if ingredient.valid?
-      ingredient.save
-      redirect_to recipe_path(id: ingredient.recipe.id), status: 303
+    @ingredient = RecipeFood.find(ingredient_id)
+    @ingredient.quantity = quantity
+    if @ingredient.valid?
+      @ingredient.save
+      respond_to do |format|
+        format.html {redirect_to recipe_path(id: @ingredient.recipe.id), status: 303}
+        format.turbo_stream { flash.now[:notice] =  "Ingredient  #{@ingredient.food.name} updated successfully"}
+      end
       return
     end
     redirect_to recipe_path(id: params[:recipe_id]), status: 303
